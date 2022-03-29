166c0a2a4c14720ec0292b1dcad7b07cb8a17fea
Giulio Romualdi <giulio.romualdi@gmail.com>
2022-03-29 11:23:32 +0200

    Use pathlib in setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,14 @@
 # Released under the terms of the BSD 3-Clause License
 
 from setuptools import setup
-from glob import glob
-import os
+from pathlib import Path
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    all_files = glob('./iCub/**/*.*', recursive=True)
-    data_files = [(os.path.join('share', os.path.split(path)[0]),
-                   [os.path.join(os.path.split(path)[0], os.path.split(path)[1])])
-                  for path in all_files]
+    data_files = [
+        (str("share" / path.parent), [str(path)])
+        for path in Path("iCub").glob("**/*.*")
+    ]
 
-    setup(data_files = data_files)
+    setup(data_files=data_files)
