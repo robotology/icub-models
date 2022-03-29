b14ca2584542e4b5d39bb59f6b719bcf5cf6094a
Giulio Romualdi <giulio.romualdi@gmail.com>
2022-03-29 10:12:03 +0200

    Use pathlib in icub_models.py

--- python/icub_models/icub_models.py
+++ python/icub_models/icub_models.py
@@ -2,28 +2,32 @@
 # @authors Giulio Romualdi
 # @copyright 2022 Istituto Italiano di Tecnologia Released under the terms of the Creative Commons Attribution Share Alike 4.0 International
 
-from os import listdir
-from os.path import dirname, join, isdir, exists, realpath
+import pathlib
 from typing import List
 
 
-def get_models_path() -> str:
-    relative_path = '@ICUB_MODELS_CXX_PYTHON_INSTALL_RELATIVE_DIR@'
+def get_models_path() -> pathlib.Path:
+    root = ""
+    relative_path = "@ICUB_MODELS_CXX_PYTHON_INSTALL_RELATIVE_DIR@"
     # If relative_path starts with @, it is a not escaped string
     # from CMake, i.e. that means that the file was installed via setup.py
     # let's substitute relative_path with its default value
-    if relative_path.startswith('@'):
-        relative_path = '../../../..'
-    root = realpath(join(dirname(__file__), relative_path))
-    return join(root, 'share', 'iCub', 'robots')
+    if relative_path.startswith("@"):
+        root = pathlib.Path(__file__).parent.parent.parent.parent.parent
+    else:
+        root = pathlib.Path(__file__) / relative_path
+
+    return (root / "share" / "iCub" / "robots").resolve()
+
 
 def get_robot_names() -> List[str]:
-    return listdir(get_models_path())
+    return [d.name for d in get_models_path().iterdir() if d.is_dir()]
 
-def get_model_file(robot_name: str) -> str:
-    model_file = join(get_models_path(), robot_name, 'model.urdf')
 
-    if exists(model_file):
+def get_model_file(robot_name: str) -> pathlib.Path:
+    model_file = get_models_path() / robot_name / "model.urdf"
+
+    if model_file.is_file():
         return model_file
     else:
-        return ''
+        return pathlib.Path("...")
